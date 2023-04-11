4b5a8e7e144803fdc39bdc6fe03b18c332bd4d7c
Paulina Szamraj <paulinaps.93@wp.pl>
2023-04-11 16:50:51 +0200

    added js formulas

--- js/script.js
+++ js/script.js
@@ -1,41 +1,55 @@
-let formElement = document.querySelector(".js-form");
-let currencyElement = document.querySelector(".js-currency");
-let amountElement = document.querySelector(".js-amount");
-let resultElement = document.querySelector(".js-result");
+{
+  const calculateResult = (amount, currency) => {
+    const currencyDolar = 4.2855;
+    const currencyEuro = 4.6785;
+    const currencyFrank = 4.6989;
+    const currencyFunt = 5.3462;
+    const currencyKorona = 0.1996;
+    const currencyYuan = 5.7885;
 
-let currencyDolar = 4.2855;
-let currencyEuro = 4.6785;
-let currencyFrank = 4.6989;
-let currencyFunt = 5.3462;
-let currencyKorona = 0.1996;
-let currencyYuan = 5.7885;
+    switch (currency) {
+      case "dolar":
+        return amount / currencyDolar;
 
-formElement.addEventListener("submit", (event) => {
-  event.preventDefault();
+      case "euro":
+        return amount / currencyEuro;
 
-  let currency = currencyElement.value;
-  let amount = amountElement.value;
-  let result = resultElement.value;
+      case "frank":
+        return amount / currencyFrank;
 
-  switch (currency) {
-    case "dolar":
-      result = amount / currencyDolar;
-      break;
-    case "euro":
-      result = amount / currencyEuro;
-      break;
-    case "frank":
-      result = amount / currencyFrank;
-      break;
-    case "funt":
-      result = amount / currencyFunt;
-      break;
-    case "korona":
-      result = amount / currencyKorona;
-      break;
-    case "yuan":
-      result = amount / currencyYuan;
-      break;
+      case "funt":
+        return amount / currencyFunt;
+
+      case "korona":
+        return amount / currencyKorona;
+
+      case "yuan":
+        return amount / currencyYuan;
+    }
+  };
+
+  const updateResultText = (result) => {
+    const resultElement = document.querySelector(".js-result");
+    resultElement.innerText = result.toFixed(2);
   }
-  resultElement.innerText = result.toFixed(2);
-});
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const currencyElement = document.querySelector(".js-currency");
+    const amountElement = document.querySelector(".js-amount");
+
+    const result = calculateResult(+amountElement.value, currencyElement.value);
+
+    updateResultText(result);
+  };
+
+  const init = () => {
+    const formElement = document.querySelector(".js-form");
+
+    formElement.addEventListener("submit", onFormSubmit)
+  };
+
+  init();
+
+}
